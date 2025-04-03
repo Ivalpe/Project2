4a296650c3e66ebec7cb0074b135604611aed3d6
0psycada <157905893+0psycada@users.noreply.github.com>
2025-04-03 17:18:38 +0200

    ajustes de la jana no se
    Co-Authored-By: Jana Puig Solà <160216332+JanaPuig@users.noreply.github.com>

--- PlatformGame-L13_Pathfinding_AStar_Solution/src/Scene.cpp
+++ PlatformGame-L13_Pathfinding_AStar_Solution/src/Scene.cpp
@@ -111,7 +111,7 @@ bool Scene::Update(float dt)
 	int Py = player->position.getY();
 
 	Engine::GetInstance().render.get()->camera.x = -(Px - 600)*2;
-	Engine::GetInstance().render.get()->camera.y = -(Py-400)*2;
+	Engine::GetInstance().render.get()->camera.y = -(Py-300)*2;
 
 	//if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
 	//	Engine::GetInstance().render.get()->camera.y -= ceil(camSpeed * dt);
