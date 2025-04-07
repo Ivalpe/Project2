47c02ba20c661c7ac4765b3e07396b2794c096b6
Jana Puig Solà <160216332+JanaPuig@users.noreply.github.com>
2025-04-07 13:37:07 +0200

    Revert "The beach"
    This reverts commit 3987af4f4ef7a5a5a3bee51315847fba5bb2d21f.

--- PlatformGame-L13_Pathfinding_AStar_Solution/Assets/Maps/BG_Base.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="BG_Base" tilewidth="32" tileheight="32" tilecount="28560" columns="420">
- <image source="BG_Base.png" width="13440" height="2176"/>
-</tileset>
--- PlatformGame-L13_Pathfinding_AStar_Solution/Assets/Maps/Tilemap_Beach.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tilemap_Beach" tilewidth="32" tileheight="32" tilecount="720" columns="30">
- <image source="Tilemap_Beach.png" width="989" height="769"/>
-</tileset>
